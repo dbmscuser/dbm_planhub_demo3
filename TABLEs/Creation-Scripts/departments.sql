5cb2a24ae6ecea37c69c0612036eda8e40cbd0b5
dbmscuser <dbmscuser>
2025-04-28 12:53:40 +0000

    departments

---
+++ TABLEs/Creation-Scripts/departments.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `departments` (
+  `dept_id` int NOT NULL,
+  `dept_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`dept_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
